ccdb0100d0e6aa62c34cff902cee7630649df4af
Kelemen Gábor <kgabesz18@gmail.com>
2020-07-08 17:32:49 +0200

    completed the fourth assignment

---
+++ main4.js
@@ -0,0 +1,15 @@
+var canvas = document.getElementById('myCanvasFour');
+var context = canvas.getContext('2d');
+
+context.beginPath();
+context.moveTo(0, 0);
+context.lineTo(450, 300);
+context.strokeStyle = 'red';
+context.stroke();
+
+context.beginPath();
+context.moveTo(0, 300);
+context.lineTo(450, 0);
+context.strokeStyle = 'red';
+context.stroke();
+
